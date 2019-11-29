1475343ffc46b0de2db0541b21adc39a6639fddd
Afzal Hassen <afzal.hassen@gmail.com>
2019-11-29 12:43:52 +0200

    Update cp Year

--- SOLIDplate.AssemblyInfo.cs
+++ SOLIDplate.AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyCompany("Afzal Hassen")]
-[assembly: AssemblyCopyright("Copyright © 2017 Afzal Hassen (MIT License)")]
+[assembly: AssemblyCopyright("Copyright © 2019 Afzal Hassen (MIT License)")]
 [assembly: AssemblyTrademark("SOLIDplate")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
